84e247d157afead27bd3a8a0a77bb43666b2376f
Tamara Cruz <tame101@Tamaras-MacBook-Pro.local>
2015-04-17 11:05:26 -0600

    finished assignment

--- practice.js
+++ practice.js
@@ -9,11 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+  inner();
 
 
 
@@ -32,8 +32,8 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
-
+var call = callFriend();
+call('435-215-9248');
 
 
 //Next Problem
@@ -43,8 +43,13 @@ var callFriend = function(){
 /*
   Write a function called makeCounter that makes the following code work properly.
 */
-
-  //Code Here
+  var makeCounter = function() {
+    var count = 1;
+      return function() {
+      console.log(count)
+      count++;
+    }
+  };
   var count = makeCounter();
   count() // 1
   count() // 2
@@ -64,6 +69,36 @@ var callFriend = function(){
   After the function has been called N number of times, console.log('STAHHP');
 */
 
+var firstFn = function(cb, num) {
+  var count = 0
+  return function() {
+    if(count === 0){
+      cb()
+      count++;
+    }
+  }
+};
 
+var newFn = firstFn(function() {
+  console.log('woo')
+})
 
+newFn();
 
+var firstFn = function(cb, num) {
+  var count = 0
+  return function() {
+    if(count < num){
+      cb()
+      count++;
+    } else {
+      console.log('STAHHP')
+    }
+  }
+};
+
+var newFn = firstFn(function() {
+  console.log('woo')
+}, 5)
+
+newFn();
